be81104c8b2eaf4c199eb5727f60f57187c578b5
Gift Irusa <137901838+Giftirusa@users.noreply.github.com>
2023-09-15 00:59:58 +0300

    Update settings.py

--- DataForm/settings.py
+++ DataForm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-qy8b9)0_214x)#%)q_(k(j0k(jn-xw=&*duz3ajf=^$5sz=%92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

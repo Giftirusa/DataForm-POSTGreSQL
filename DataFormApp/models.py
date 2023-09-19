516d7f8f8a39caca8f31052f5657ceee35334685
Gift Irusa <137901838+Giftirusa@users.noreply.github.com>
2023-09-19 18:22:03 +0300

    Update models.py

--- DataFormApp/models.py
+++ DataFormApp/models.py
@@ -7,3 +7,8 @@ class Contribution(models.Model):
 
     def __str__(self):
         return f"${self.amount} USD by {self.contributor_email}"
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.createdAt})"

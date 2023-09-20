27d828bf01bd2755928b7726ca1044f5418eca0b
Gift Irusa <137901838+Giftirusa@users.noreply.github.com>
2023-09-20 14:13:13 +0300

    Update models.py

--- DataFormApp/models.py
+++ DataFormApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Contribution(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     contributor_email = models.EmailField()
+    createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"${self.amount} USD by {self.contributor_email}"

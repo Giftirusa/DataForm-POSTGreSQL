ed879aec0edf1553a03ba7f50adcfdd4fd2633e6
Gift Irusa <137901838+Giftirusa@users.noreply.github.com>
2023-09-20 14:04:50 +0300

    Update 0001_initial.py

--- DataFormApp/migrations/0001_initial.py
+++ DataFormApp/migrations/0001_initial.py
@@ -17,7 +17,6 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('contributor_email', models.EmailField(max_length=254)),
-                ('createdAt', models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
